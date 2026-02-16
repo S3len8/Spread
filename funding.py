29ac07270d181be9a8e3fb8e00968f4293f64af0
Denys <glushychden.10@gmail.com>
2026-02-16 16:21:01 +0200

    Clean code for start working.

--- funding.py
+++ funding.py
@@ -3,14 +3,7 @@ from symbol import FEES, set_all_symbols_funding, binance_funding, bybit_funding
 
 # Function get funding from binance and bybit in set coins_after_comparison
 def get_funding(binance: dict, bybit: dict, bitget: dict, mexc: dict, kucoin: dict, gate: dict) -> dict:
-    result = {
-        6: {},  # Coin in 6
-        5: {},  # Coin in 5
-        4: {},  # Coin in 4
-        3: {},  # Coin in 3
-        2: {},  # Coin in 2
-        1: {},  # Coin in 1
-    }  # <class 'dict'>
+    result = {}  # <class 'dict'>
     for symbol in set_all_symbols_funding:
         exchanges = {}
         if symbol in binance and binance[symbol].get('funding') is not None:  # Getting and calculation funding in percent from Binance
@@ -52,19 +45,3 @@ for key in [6, 5, 4, 3, 2, 1]:
     print(f"{key} exchanges: {funding.get(key)}")
 
 
-# def get_better_funding():
-#     result = {}
-#     for count, symbols in funding.items():
-#         filtered_symbols = {}
-#         for symbol, exchanges in symbols.items():
-#             if any(abs(funding) >= 0.9 for funding in exchanges.values()):
-#                 filtered_symbols[symbol] = exchanges
-#
-#         if filtered_symbols:
-#             result[count] = filtered_symbols
-#
-#     return result
-
-
-# better_funding = get_better_funding()  # <class 'dict'>
-# print(better_funding)
