5615ccbf980a4934b90bbf60b68c7fed2a2b035e
Denys <glushychden.10@gmail.com>
2026-02-18 23:57:34 +0200

    Add simple version of click processing and give result to bot from file calculation.

--- main.py
+++ main.py
@@ -1,23 +1,56 @@
 import aiogram
+from calculation import calculation
 import asyncio
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import Command
+from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 TOKEN = '8209170851:AAGViuYiZsc7O2m_P-yoMYDKOVmfPcoOZJ4'
-CHAT_ID = 655882078
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
 
+# Button
+keyboard = InlineKeyboardMarkup(
+    inline_keyboard=[
+        [InlineKeyboardButton(text="📊 Active Spread", callback_data="show_spread")]
+    ]
+)
 
+
+# Start
 @dp.message(Command('start'))
 async def start(message: types.Message):
-    await message.answer('Bjk;')
+    await message.answer('Choose activity:', reply_markup=keyboard)
 
 
-@dp.message
-async def get_chat_id(message: types.Message):
-    print("CHAT ID:", message.chat.id)
+# Button click processing
+@dp.callback_query()
+async def callback_handler(callback: types.CallbackQuery):
+
+    if callback.data == "show_spread":
+
+        data = calculation
+
+        text = ""
+
+        for symbol, value in data.items():
+            text += (
+                f"🚀 {symbol}\n"
+                f"Buy on: {value['buy_on']}\n"
+                f"Sell on: {value['sell_on']}\n"
+                f"Funding buy: {value['funding buy_on']}\n"
+                f"Funding sell: {value['funding sell_on']}\n"
+                f"Volume buy 24H: {value['volume_buy_24H']}\n"
+                f"Volume sell 24H: {value['volume_sell_24H']}\n"
+                f"Spread: {value['spread']}\n\n"
+            )
+
+        if not text:
+            text = "No spreads for this moment"
+
+        await callback.message.answer(text)
+        await callback.answer()
 
 
 async def main():
