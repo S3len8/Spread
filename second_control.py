c521d71d5cd7a34a643a4460661e94344f035f04
Denys <glushychden.10@gmail.com>
2026-03-04 20:55:56 +0200

    Second_control now ended, because this method to compare symbols not good, minQty and stepSize can be same, but coins different

--- second_control.py
+++ second_control.py
@@ -28,13 +28,56 @@ def get_minQty_stepSize_bybit(symbols: list) -> dict:
     Function for getting minQty and stepSize for coins after calculation
     Need for second check and compare symbols
     """
+    symbols_set = set(symbols)
     result = {}
     data = requests.get(BYBIT_MIN_SIZE).json()
     for one_symbol in data['result']['list']:
-        for symbol in symbols:
+        symbol = one_symbol['symbol']
+        if symbol in symbols_set:
             minQty = one_symbol['lotSizeFilter']['minOrderQty']
             stepSize = one_symbol['lotSizeFilter']['qtyStep']
             result[symbol] = {
+                'minQty': minQty,
+                'stepQty': stepSize,
+            }
+    return result
+
+
+def get_minQty_stepSize_bitget(symbols: list) -> dict:
+    """
+    Function for getting minQty and stepSize for coins after calculation
+    Need for second check and compare symbols
+    """
+    symbols_set = set(symbols)
+    result = {}
+    data = requests.get(BITGET_MIN_SIZE).json()
+    for one_symbol in data['data']:
+        symbol = one_symbol['symbol']
+        if symbol in symbols_set:
+            minQty = one_symbol['minTradeNum']
+            stepSize = one_symbol['sizeMultiplier']
+            result[symbol] = {
+                'minQty': minQty,
+                'stepQty': stepSize,
+            }
+    return result
+
+
+def get_minQty_stepSize_mexc(symbols: list) -> dict:
+    """
+    Function for getting minQty and stepSize for coins after calculation
+    Need for second check and compare symbols
+    """
+    symbols_set = set(symbols)
+    result = {}
+    data = requests.get(MEXC_MIN_SIZE).json()
+    for one_symbol in data['data']:
+        symbol = one_symbol['symbol']
+        normalized_symbol = symbol.replace('_USDT', 'USDT')
+        if normalized_symbol in symbols_set:
+            minQty = one_symbol['contractSize']
+            stepSize = one_symbol['contractSize']  # Don`t found step size for mexc
+            result[normalized_symbol] = {
                 'minQty': minQty,
                 'stepQty': stepSize,
             }
@@ -48,7 +91,9 @@ def get_symbols_calculation() -> dict:
     symbols = ['FUNUSDT', 'BTCUSDT']
     contracts_binance = get_minQty_stepSize_binance(symbols)
     contracts_bybit = get_minQty_stepSize_bybit(symbols)
-    return contracts_binance, contracts_bybit
+    contracts_bitget = get_minQty_stepSize_bitget(symbols)
+    contracts_mexc = get_minQty_stepSize_mexc(symbols)
+    return f' binance: {contracts_binance}, bybit: {contracts_bybit}, bitget: {contracts_bitget}, mexc: {contracts_mexc}'
 
 
 def compare_symbols():
